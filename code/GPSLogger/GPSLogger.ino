e8e368f308a7351f9d29adaa5c2ec7386193f649
Trevor Stanhope <trevor@T510>
2013-07-11 22:52:55 -0400

    GPSLogger and docs

---
+++ code/GPSLogger/GPSLogger.ino
@@ -0,0 +1,42 @@
+/* --- Header --- */
+#include <SoftwareSerial.h>
+#include <SD.h>
+#define SD_PIN 10
+#define GPS_TX_PIN 2 // TxD jumper position on shield
+#define GPS_RX_PIN 3 // RxD jumper position on shield
+#define BAUD 9600
+
+/* --- Declarations --- */
+SoftwareSerial gps(GPS_TX_PIN, GPS_RX_PIN);
+
+/* --- Setup --- */
+void setup() {
+  Serial.begin(BAUD);
+  pinMode(SD_PIN, OUTPUT);
+  SD.begin(SD_PIN);
+  gps.begin(BAUD);
+}
+
+/* --- Loop --- */
+void loop() {
+  char index = 0;
+  char temp = 0;
+  String dataString = "";
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  if (dataFile) {
+    while(gps.available()) {
+      temp = gps.read();
+      dataString += String(temp);
+      index++;
+      if (index > 200) {
+        break;
+      }
+    }
+  }
+  if (dataString != "") {
+    Serial.println(dataString);
+  }
+  dataFile.print(dataString);
+  dataFile.close();
+}
+
